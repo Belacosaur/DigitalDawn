a9f45abc1737293869276044ed30e32f5393f876
Caleb Martin <calebjmartin@hotmail.com>
2024-08-20 14:43:31 +0800

    Next Draft

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-// script.js
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+    });
+});
+
+$(document).ready(function() {
+    $('#hero-carousel').carousel({
+        interval: 5000 // 5 seconds
+    });
+
+    $('#mobile-menu-btn').on('click', function() {
+        $('#nav-links').slideToggle();
     });
 });
